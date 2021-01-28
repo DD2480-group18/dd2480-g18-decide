fc01b020de742063680a3bc5421052ed37d32d53
Kalle Pettersson <kalpet@kth.se>
2021-01-28 15:25:07 +0100

    changed function signiture

--- components/LIC2.java
+++ components/LIC2.java
@@ -7,33 +7,34 @@ import java.lang.Math;
 public class LIC2 {
     public static void main(String[] args) {
         LIC2 l = new LIC2();
-        l.getLic2(3.1415926535, 2);
+        double[] xCordinates = {0,0,-1,3,6,3,6,8,6,53,1};
+        double[] yCordinates = {1,0,0,6,8,0,23,6,3,1,2};
+        l.getLic2(xCordinates,yCordinates,xCordinates.length, 3.1415926535, 2);
     }
 
-    public boolean getLic2(double PI, double EPSILON){
+    public boolean getLic2(double [] xPos, double[] yPos, int numPoints,  double PI, double EPSILON){
 
-        double[] xCordinates = {0,0,-1,3,6,3,6,8,6,53,1};
-        double[] yCordinates = {1,0,0,6,8,0,23,6,3,1,2}; 
+         
 
         double x1,x2,y1,y2;
         double angle=-1;
         double lenX;
         double leny;
-        for (int i = 0; i < yCordinates.length-2; i++) {
+        for (int i = 0; i < numPoints-2; i++) {
 
-            if((xCordinates[i] == xCordinates[i+1] && yCordinates[i] == yCordinates[i+1])||(xCordinates[i+2] == xCordinates[i+1] && yCordinates[i+2] == yCordinates[i+1])){
+            if((xPos[i] == xPos[i+1] && yPos[i] == yPos[i+1])||(xPos[i+2] == xPos[i+1] && yPos[i+2] == yPos[i+1])){
                 continue;
             }
 
             // vector BA
-            x1 = xCordinates[i] - xCordinates[i+1];
-            x2 = yCordinates[i] - yCordinates[i+1];
+            x1 = xPos[i] - xPos[i+1];
+            x2 = yPos[i] - yPos[i+1];
             // vector BC
-            y1 = xCordinates[i+2] - xCordinates[i+1];
-            y2 = yCordinates[i+2] - yCordinates[i+1];
-            System.out.println("A(" + xCordinates[i] + "," + yCordinates[i] + ")");
-            System.out.println("B(" + xCordinates[i+1] + "," + yCordinates[i+1] + ")");
-            System.out.println("C(" + xCordinates[i+2] + "," + yCordinates[i+2] + ")");
+            y1 = xPos[i+2] - xPos[i+1];
+            y2 = yPos[i+2] - yPos[i+1];
+            System.out.println("A(" + xPos[i] + "," + yPos[i] + ")");
+            System.out.println("B(" + xPos[i+1] + "," + yPos[i+1] + ")");
+            System.out.println("C(" + xPos[i+2] + "," + yPos[i+2] + ")");
             System.out.println("x1: " + x1 + " x2: " + x2 +"y1: " + y1 + " y2: " + y2);
             lenX = Math.sqrt(Math.pow(x1, 2) + Math.pow(x2, 2));
             leny = Math.sqrt(Math.pow(y1, 2) + Math.pow(y2, 2)); 
