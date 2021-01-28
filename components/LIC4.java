6a3eab9c0bf0b75f61bd10a239a5b4c12dc7d2c7
Lukas Gutenberg <lukasgut@kth.se>
2021-01-28 17:10:26 +0100

    #9 code done and one test

---
+++ components/LIC4.java
@@ -0,0 +1,36 @@
+public class LIC4 {
+	public boolean lic4(int q_pts, int quads, double[] X, double[] Y){
+		for (int i = 0; i <= X.length-q_pts; ++i){
+			boolean[] taken = {false, false, false, false};
+			for (int j = 0; j < q_pts; j++) {
+				if (X[i+j] >= 0){
+					if (Y[i+j] >= 0){
+						taken[0] = true;
+					}
+					else if (X[i+j] == 0){ //quadrant priority
+						taken[2] = true;
+					}
+					else{
+						taken[3] = true;
+					}
+				}
+				else {
+					if (Y[i+j] >= 0){
+						taken[1] = true;
+					}
+					else {
+						taken[2] = true;
+					}
+				}
+			}
+			int c = 0;
+			for (int j = 0; j < 4; j++) {
+				if (taken[j]){
+					c++;
+				}
+			}
+			if (c > quads) return true;
+		}
+		return false;
+	}
+}
