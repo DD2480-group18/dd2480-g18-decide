a4e24b8bfcc8238ad165c74f1caa4169b66f0d7b
Lukas Gutenberg <lukasgut@kth.se>
2021-01-29 14:37:59 +0100

    fix #10 fixed small bug in testing now ready for merge

--- components/tests/LIC9Test.java
+++ components/tests/LIC9Test.java
@@ -24,7 +24,7 @@ public class LIC9Test {
     void trueTest() {
         double[] xs = {2,0,0,1,0,1};
         double[] ys = {2,0,0,1,0,3};
-        LIC0 lic9 = new LIC9();
+        LIC9 lic9 = new LIC9();
         assertTrue(lic9.getLic9(xs,ys,xs.length,2,1,3.1415926535, 2));
     }
 }
