3b1af10db939bbe146eae4318f07e13729c4f177
Kalle Pettersson <kalpet@kth.se>
2021-01-29 13:48:02 +0100

    #10 edited an incorrect unit test

--- components/tests/LIC9Test.java
+++ components/tests/LIC9Test.java
@@ -24,8 +24,7 @@ public class LIC9Test {
     void trueTest() {
         double[] xs = {2,0,0,1,0,1};
         double[] ys = {2,0,0,1,0,3};
-        double len = 4;
-        LIC0 lic9 = new LIC9(xs, ys, len);
+        LIC0 lic9 = new LIC9();
         assertTrue(lic9.getLic9(xs,ys,xs.length,2,1,3.1415926535, 2));
     }
 }
