5cd1780d6088bf5503dda24a59bd960aec3080fd
oscar <osclind@kth.se>
2021-02-01 14:55:04 +0100

    Feat/#38
    Copy LIC12.java and LIC12Test.java to src

---
+++ src/test/java/group18/LIC/LIC12Test.java
@@ -0,0 +1,42 @@
+package group18.LIC;
+
+import main.java.group18.LIC.LIC12;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class LIC12Test {
+
+    /**
+     * Tests an input set that should evaluate to true
+     */
+    @Test
+    void LIC12trueTest() {
+        double[] xs = {-4, -3, -1, -1, 1, 1, 3, 3, 6, 6};
+        double[] ys = {-1, 1, -1, 3, 1, 4, -2, 2, 1, 2};
+        double LENGTH1 = 5;
+        double LENGTH2 = 2;
+        int NUM_POINTS = xs.length;
+        int K_PTS = 1;
+        assertTrue(LIC12.getCondition(xs, ys, NUM_POINTS, K_PTS, LENGTH1, LENGTH2));
+        LIC12.setCondition1(false);
+        LIC12.setCondition2(false);
+    }
+
+    /**
+     * Tests an input set that should evaluate to false
+     */
+    @Test
+    void LIC12falseTest1() {
+        double[] xs = {-4, -3, -1, -1, 1, 1, 3, 3, 6};
+        double[] ys = {-1, 1, -1, 3, 1, 4, -2, 2, 1};
+        double LENGTH1 = 5;
+        double LENGTH2 = 2;
+        int NUM_POINTS = xs.length;
+        int K_PTS = 1;
+        assertFalse(LIC12.getCondition(xs, ys, NUM_POINTS, K_PTS, LENGTH1, LENGTH2));
+        LIC12.setCondition1(false);
+        LIC12.setCondition2(false);
+    }
+}
