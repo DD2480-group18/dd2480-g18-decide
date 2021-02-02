9e36cb37e2519755da0fde2c009e7df4291d48c7
oscar <osclind@kth.se>
2021-02-02 08:31:34 +0100

    feat/#30
    Add Launch + tests to src-package

---
+++ src/main/java/group18/Launch.java
@@ -0,0 +1,9 @@
+package group18;
+
+import java.util.stream.IntStream;
+
+public class Launch {
+    public static boolean launch(boolean[] FUV) {
+        return FUV.length > 0 ? IntStream.range(0, FUV.length).allMatch(i -> FUV[i]) : false;
+    }
+}
